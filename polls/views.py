9ee0b3648d7310b24e3b75e5ff208fdabfb64fcd
Pakpoom-Phoonpunt <s6201012620210@email.kmutnb.ac.th>
2021-05-04 12:50:59 +0700

    completed detail template

--- polls/views.py
+++ polls/views.py
@@ -16,4 +16,17 @@ def display(request):
     context = {'latest_question_list': Question.objects.all()}
     return render(request,"polls/index.html",context)
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
